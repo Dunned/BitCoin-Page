f704141dda9f0664c9f56a0e0a5af4b81dc7626a
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2022-01-31 19:38:45 -0500

    Finish

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
+    ScrollReveal().reveal('.headline')
+    ScrollReveal().reveal('.tagline', { delay: 600 })
+    ScrollReveal().reveal('.product-detail--card', { interval: 500 });
     siguiente=document.getElementById('btnNext');
     bSiguiente=document.getElementById('bnext');
     anterior=document.getElementById('btnBack');
